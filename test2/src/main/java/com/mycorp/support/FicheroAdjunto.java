430074d2b88c9239fecb2d7f9762b34950d56f5a
Ángel Laguna Chico <alaguna@atsistemas.com>
2021-02-01 15:50:34 +0100

    First commit

--- test2/src/main/java/com/mycorp/support/FicheroAdjunto.java
+++ test2/src/main/java/com/mycorp/support/FicheroAdjunto.java
@@ -2,6 +2,7 @@
 package com.mycorp.support;
 
 import java.io.Serializable;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
